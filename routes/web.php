17f7879ef8a26f7d49079ec7489d1f676aaeccc2
Yago Jesus <yyagoyy.jesus@gmail.com>
2026-09-06 11:20:27 -0300

    terceira alteração

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', [App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
 
-Route::get('/', [App\Http\Controllers\PrincipalController::class, 'principal']);
+Route::get('/sobre-nos', [App\Http\Controllers\SobreNosController::class, 'sobreNos'])->name('site.sobre-nos');
+Route::get('/contato', [App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/login', function() { return 'Login'; })->name('site.login');
 
-Route::get('/sobre-nos', [App\Http\Controllers\SobreNosController::class, 'sobreNos']);
+Route::prefix('/app')->group(function() {
+    Route::get('/clientes', function() { return 'Clientes'; })->name('site.clientes');
+    Route::get('/fornecedores', function() { return 'Fornecedores'; })->name('site.fornecedores');
+    Route::get('/produtos', function() { return 'Produtos'; })->name('site.produtos');
+});
 
-Route::get('/contato', [App\Http\Controllers\ContatoController::class, 'contato']);
+Route::get('/rota1', function() {echo 'rota 1'; })->name('site.rota1');
+    
+Route::get('/rota2', function() { return redirect()->route('site.rota1'); })->name('site.rota2');
+//Route::redirect('rota2', 'rota1');
+
+Route::fallback(function() {
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">Clique aqui</a> para ir para a página inicial.';
+});
+
+
 
